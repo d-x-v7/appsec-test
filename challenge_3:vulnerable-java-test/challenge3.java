42b7914b6a65427bd231292397a278eaa5b463f4
Simon Wilkins <siwilkins@overstock.com>
2021-04-08 17:21:26 -0600

    Revamped challenge for new candidates

--- challenge_3:vulnerable-java-test/challenge3.java
+++ challenge_3:vulnerable-java-test/challenge3.java
@@ -1,5 +1,19 @@
-//Example case
+//Example case 1
 String sql_query = "SELECT * FROM users WHERE userid ='"+ username + "'" + " AND password='" + Base64.getEncoder().encodeToString(passwordString.getBytes()) + "'";
 Statement stmt = connection.createStatement();
 ResultSet rs = stmt.executeQuery(query);
 //End of example
+
+//Example case 2
+public void postToMessageBoard(MessageBoard mb){
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	String s = br.readLine(); //VIOLATION
+	mb.post(s)
+}
+//End of Example
+
+//Example case 3
+String url = request.getParameter("hidden_url"); // not so hidden...
+URL url = new URL(url); // VIOLATION 
+InputStream is = url.openConnection().getContent();
+//End of Example
