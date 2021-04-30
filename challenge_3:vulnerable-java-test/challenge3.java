e48670e7505fe27cf0bffad82870718389ed31bc
Dylan <22502736+d-x-v7@users.noreply.github.com>
2021-04-29 21:09:38 -0600

    Updated challenge-notes.txt and made progress on the challenges

--- challenge_3:vulnerable-java-test/challenge3.java
+++ challenge_3:vulnerable-java-test/challenge3.java
@@ -1,5 +1,6 @@
 //Example case 1
-String sql_query = "SELECT * FROM users WHERE userid ='"+ username + "'" + " AND password='" + Base64.getEncoder().encodeToString(passwordString.getBytes()) + "'";
+// ORIGINAL -- String sql_query = "SELECT * FROM users WHERE userid ='"+ username + "'" + " AND password='" + Base64.getEncoder().encodeToString(passwordString.getBytes()) + "'";
+String sql_query = "SELECT * FROM users WHERE userid = ?"+ username + "'" + " AND password='" + Base64.getEncoder().encodeToString(passwordString.getBytes()) + "'";
 Statement stmt = connection.createStatement();
 ResultSet rs = stmt.executeQuery(query);
 //End of example
@@ -14,6 +15,6 @@ public void postToMessageBoard(MessageBoard mb){
 
 //Example case 3
 String url = request.getParameter("hidden_url"); // not so hidden...
-URL url = new URL(url); // VIOLATION 
+URL url = new URL(url); // VIOLATION
 InputStream is = url.openConnection().getContent();
 //End of Example
